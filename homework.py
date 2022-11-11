2fd3872e696dddc9968a891f480dc3b797a99591
AvailableNow <currently.out.of.service@gmail.com>
2022-11-11 15:45:39 -0500

    Version5

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, asdict, fields
+from dataclasses import dataclass, asdict
 
 
 @dataclass
@@ -133,10 +133,6 @@ WORKOUT_TYPES = {
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    if workout_type not in WORKOUT_TYPES:
-        raise ValueError('Parameter "workout_type" is not in the dictionaly')
-    elif len(data) != len(fields(WORKOUT_TYPES[workout_type])):
-        raise ValueError('Incorrect number of arguments submitted to the class')
     return WORKOUT_TYPES[workout_type](*data)
 
 
